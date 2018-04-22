6b061b6750fa85b99d900c8d4af43a65e3bc0829
Zyf210304 <672233580@qq.com>
2018-04-22 13:06:54 +0800

    Initial Commit

---
+++ YFDemo_ Macro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFDemo_ Macro
+//
+//  Created by 亚飞 on 2018/4/22.
+//  Copyright © 2018年 yafei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
